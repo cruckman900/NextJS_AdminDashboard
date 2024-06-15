d3bab7096729e7e965304cc0950e8526b0fd15c8
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-06-15 01:42:50 -0400

    created sidebar

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import MenuLink from "./menuLink/menuLink";
 import styles from "./sidebar.module.css";
 import {
@@ -79,7 +80,14 @@ const menuItems = [
 const Sidebar = () => {
     return (
         <div className={styles.container}>
-            <ul>
+            <div className={styles.user}>
+                <Image className={styles.userImage} src="/noavatar.png" alt="" width="50" height="50" />
+                <div className={styles.userDetail}>
+                    <span className={styles.username}>John Doe</span>
+                    <span className={styles.userTitle}>Administrator</span>
+                </div>
+            </div>
+            <ul className={styles.list}>
                 {menuItems.map(cat => (
                     <li key={cat.title}>
                         <span className={styles.cat}>{cat.title}</span>
